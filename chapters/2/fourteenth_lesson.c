d8b94cc0000fddd22cd21934fbb08e84e334af2d
nefir2 <vadim.ahtamov@gmail.com>
2023-12-08 01:05:40 +0300

    not the end of lesson

--- chapters/2/fourteenth_lesson.c
+++ chapters/2/fourteenth_lesson.c
@@ -15,5 +15,15 @@ int main(void) {
 	
 	// * - idk what it doing
 	
+	//for %d:
+	//	%hd - short int
+	//	%ld - long int
+	//	%lf, %le - long double
+	int someInt;
+	printf("input some int: ");
+	scanf("%d", &someInt);
+	
+	printf("some int is equals: %d", someInt);	
+
 	return 0;
 }
