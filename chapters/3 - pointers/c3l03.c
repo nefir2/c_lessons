0005627f396733ffea5510d342fc472666e415ca
nefir2 <vadim.ahtamov@gmail.com>
2023-12-12 12:28:35 +0300

    doesn't ended, test this

--- chapters/3 - pointers/c3l03.c
+++ chapters/3 - pointers/c3l03.c
@@ -9,10 +9,40 @@ int main(void) {
 	while (1) {
 		printf("ptr = %p\n*ptr = %d\n", (void*)ptr, *ptr);
 		scanf("%c", &ans);
-		if (ans == '+') ptr++;
-		else if (ans == '-') ptr--;
-		else if (ans == 'q') return 2;
+		if (ans == '+') ptr++; //++ == ptr + sizeof(typeof(*ptr))
+		else if (ans == '-') ptr--; //-- = ptr - sizeof(typeof(*ptr))
+		else if (ans == 'q') break;
 		//else return 127;
 	}
+	
+	double d = .0;
+	double* ptrd = &d;
+	printf("daddress = %p\ndvalue = %lf\n", (void*)ptrd, *ptrd);
+	ptrd++;
+	printf("daddress++:\ndaddress = %p\ndvalue = %lf\n", (void*)ptrd, *ptrd);
+
+	char c = ' ';
+	char* ptrc = &c;
+	printf("caddress = %p\ncvalue = %c\n", (void*)ptrc, *ptrc);
+	ptrc++;
+	printf("caddress++:\ncaddress = %p\ncvalue = %c\n\n", (void*)ptrc, *ptrc);
+	
+	ptrd+=2; //+ 2 = ptrd + (sizeof(typeof(*ptrd) * 2)
+	printf("daddress + 2:\ndaddress = %p\ndvalue = %lf\n\n", (void*)ptrd, *ptrd);
+
+	//ptrd += (ptrd - 1); //no.
+	
+	int y = (int)('n' + 'e' + 'w' + ' ' + 'i' + 'n' + 't');
+	int* ptr1 = &y; 
+	printf("ptr - ptr1 = %d\n\n", ptr - ptr1); //(int)(ptr - ptr1) = (ptr - ptr1) / sizeof(typeof(*ptr))
+	
+	int z = *ptr1++; //reads from right to left. (*ptr1, z = *ptr1, ptr1 = ptr1 + 1(postfix increment))
+	printf("z = %d\n", z);
+	printf("ptr1 = %p\n*ptr1 = %d\n", (void*)ptr1, *ptr1);
+	ptr1--;
+	z = (*ptr1)++; //*ptr1, z = *ptr1, *ptr1 = *ptr1 + 1
+	printf("z = %d\nptr1 = %p\n*ptr1 = %d", z, (void*)ptr1, *ptr1);
+
+
 	return 0;
 }
